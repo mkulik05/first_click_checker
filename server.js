e4cc4cb6af0b8dfd6a9622a65ceae5462188aa0a
mkulik05 <mishakulik2@gmail.com>
2020-11-23 13:47:44 +0300

    ++++

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ Object.keys(ifaces).forEach(function (ifname) {
   var alias = 0;
 
   ifaces[ifname].forEach(function (iface) {
+    console.log(console.log(iface.family , " ", iface.address))
     if ('IPv4' !== iface.family || iface.internal !== false) {
       // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
       return;
